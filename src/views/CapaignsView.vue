de527d894b4792915856677be520ebc9ae47735a
theroadfreak <dushancimbalevic@gmail.com>
2023-04-21 00:57:51 +0200

    small fix

--- src/views/CapaignsView.vue
+++ src/views/CapaignsView.vue
@@ -6,12 +6,17 @@
           class="col-12 col-sm-6 col-md-4 mb-3"
       >
         <router-link class="nav-link" :to="{name: 'campaigns-view', params: {albumName: campaign.albumName}}">
-          <img
-              v-lazy="require('@/assets/campaigns/'+campaign.thumbnail)"
-              :alt="campaign.name"
-              class="w-100"
-          >
-          {{ campaign.name }}
+          <div class="custom-hover-container">
+            <div class="custom-hover-show d-flex align-items-center justify-content-center w-100 h-100">
+              <h5 v-html="campaign.name" class="text-white text-uppercase"></h5>
+            </div>
+            <img
+                v-lazy="require('@/assets/campaigns/'+campaign.thumbnail)"
+                :alt="campaign.name"
+                class="w-100"
+            >
+          </div>
+
         </router-link>
       </div>
     </div>
@@ -42,17 +47,17 @@ import {ref} from "vue";
 
 const campaigns = ref([
   {
-    name: 'Salt Milano | Montenegro',
+    name: 'Salt Milano <br> Montenegro',
     thumbnail: 'SLM/SLM_Cover.jpg',
     albumName: "salt-milano-montenegro"
   },
   {
-    name: 'Salt Milano | Portofino',
+    name: 'Salt Milano <br> Portofino',
     thumbnail: 'SLM_Portofino/cover02.jpg',
     albumName: "salt-milano-portofino"
   },
   {
-    name: 'Salt Milano | Vourvourou Greece',
+    name: 'Salt Milano <br>Vourvourou Greece',
     thumbnail: 'SLM_Greece/cover_vv.jpg',
     albumName: "salt-milano-greece"
   },
@@ -61,4 +66,22 @@ const campaigns = ref([
 </script>
 
 <style scoped>
+.custom-hover-container {
+  position: relative;
+}
+
+.custom-hover-show {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: all 0.7s ease;
+  opacity: 0;
+  background-color: black;
+}
+
+.custom-hover-show:hover {
+  transition: all 0.7s ease;
+  background-color: rgba(0, 0, 0, 0.6);
+  opacity: 1;
+}
 </style>
